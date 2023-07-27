3569810ee441f8a7bcc6d702e301207b378f4a3a
Shooter Paulo <87977839+AngelSwagPS@users.noreply.github.com>
2023-07-27 04:06:10 +0100

    Initial commit

---
+++ lib/aichatpage.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late TextEditingController _inputController;
+  late ScrollController _scrollController;
+  late List<String> _messages;
+
+  @override
+  void initState() {
+    super.initState();
+    _inputController = TextEditingController();
+    _scrollController = ScrollController();
+    _messages = [];
+  }
+
+  @override
+  void dispose() {
+    _inputController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<String> getChatGPTResponse(String prompt) async {
+    // Replace 'YOUR_API_KEY' with your actual OpenAI API key
+    final apiKey = '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';
+    final endpoint =
+        'https://api.openai.com/v1/engines/davinci-codex/completions';
+
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $apiKey',
+    };
+
+    final data = {
+      'prompt': prompt,
+      'max_tokens': 100,
+    };
+
+    final response = await http.post(
+      Uri.parse(endpoint),
+      headers: headers,
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = jsonDecode(response.body);
+      final textResponse = responseData['choices'][0]['text'];
+      print('API Response: $textResponse');
+      return textResponse;
+    } else {
+      throw Exception('Failed to fetch response from ChatGPT');
+    }
+  }
+
+  Future<void> _submitPrompt() async {
+    final prompt = _inputController.text.trim();
+
+    if (prompt.isNotEmpty) {
+      try {
+        final response = await getChatGPTResponse(prompt);
+        setState(() {
+          _messages.add(prompt);
+          _messages.add(response);
+          _inputController.clear();
+        });
+        _scrollToBottom();
+      } catch (e) {
+        print('Error: $e');
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Error'),
+              content:
+                  const Text('Failed to get a response. Please try again.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ChatGPT'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: _messages.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(_messages[index]),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    style: TextStyle(color: Colors.black),
+                    controller: _inputController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      hintText: 'Type your message',
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SFProRegular',
+                        fontWeight: FontWeight.w300,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8.0),
+                ElevatedButton(
+                  onPressed: _submitPrompt,
+                  child: const Text(
+                    'Send',
+                    style: TextStyle(fontFamily: 'SFProRegular', fontSize: 15),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ChatPage(),
+  ));
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,222 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_import
+
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+import 'package:http/io_client.dart';
+import 'package:student_support_app/components/mybutton.dart';
+import 'package:student_support_app/home_page.dart';
+import 'colors.dart' as color;
+import 'components/textfield.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
+class LoginAlbum {
+  final String username;
+  final String token;
+
+  const LoginAlbum({
+    required this.username,
+    required this.token,
+  });
+
+  factory LoginAlbum.fromJson(Map<String, dynamic> field) {
+    return LoginAlbum(
+      username: field['username'],
+      token: field['token'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['username'] = username;
+    data['token'] = token;
+    return data;
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController userEmailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+  TextEditingController loadingcontroller = TextEditingController();
+
+  void signUserIn(BuildContext context) {
+    // Navigate to HomePage
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+    );
+  }
+
+  Future<String> readResponse(HttpClientResponse response) async{
+    final completer = Completer<String>();
+    final contents = StringBuffer();
+    response.transform(utf8.decoder).listen((data) {
+      contents.write(data);
+    }, onDone: () => completer.complete(contents.toString()));
+    return completer.future;
+  }
+
+  Future<int> check(String userEmail, String password,) async {
+    try {
+      HttpClient client = HttpClient();
+      client.badCertificateCallback =
+      ((X509Certificate cert, String host, int port) => true);
+
+      String url = 'https://192.168.198.45:7125/api/User/Login';
+
+      Map map = {
+        "userEmail": "PS@yahoo",
+        "password": "nope"
+      };
+
+      HttpClientRequest request = await client.postUrl(Uri.parse(url));
+      request.headers.set('content-type', 'application/json');
+      request.add(utf8.encode(json.encode(map)));
+      HttpClientResponse response = await request.close();
+      String reply = await response.transform(utf8.decoder).join();
+
+      print(reply);
+
+      if(response.statusCode==200){
+        //To Match THe JsonLike "reply" values to each other
+        Map<String, dynamic> valueMap = json.decode(reply);
+        //To turn the matched values into objects
+        LoginAlbum loginAlbum = LoginAlbum.fromJson(valueMap);
+        SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+        sharedPreferences.setString('token', loginAlbum.token);
+        print(loginAlbum.token);
+      }
+      client.close();
+      return response.statusCode;
+
+    }on SocketException{
+      //There is no route to server
+      return 113;
+    }
+  }
+
+  Future<int> validateUser(String userEmail, String password,) async {
+
+      var response = await http.post(
+        Uri.parse('http://192.168.198.45:5125/api/User/Login'),
+        // headers: <String, String>{
+        //   'Content-Type': 'application/json; charset=UTF-8',
+        // },
+        body: {
+          "userEmail": "PS@yahoo",
+          "password": "nope"
+        },
+      );
+      if (response.statusCode == 200) {
+        // If the server did return a 201 CREATED response,
+        // then parse the JSON.
+        return response.statusCode;
+      } else {
+        // If the server did not return a 201 CREATED response,
+        // then throw an exception.
+        //throw Exception('Failed to create album.');
+        return response.statusCode;
+      }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: color.AppColor.homePageBackground,
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Icon(
+                  Icons.support_agent,
+                  size: 100,
+                  color: Colors.blueGrey.shade900,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Welcome back buddy',
+                  style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.blueGrey.shade400),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //USERNAME FIELD
+                UserTextField(
+                  controller: userEmailcontroller,
+                  hintText: 'UserEmail',
+                  obscureText: false,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                //PASSWORD FIELD
+                UserTextField(
+                  controller: passwordcontroller,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //SUBMIT BUTTON WITH PERFORMNING AUTH AND REDIRECTING TO HOMEPAGE
+                MyButton(
+                    onTap: () async  {
+                      setState(() {
+                        loadingcontroller.text = "Loading...";
+                      });
+                        int futureCode = await check(userEmailcontroller.text, passwordcontroller.text);
+                      setState(() {
+                        loadingcontroller.text = "";
+                      });
+                    }
+                ),
+                TextField(
+                    style: TextStyle(color: Colors.white),
+                    obscureText: false,
+                    controller: loadingcontroller,
+                    enabled: false,
+                    readOnly:true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Color(0xff0d1b1c),
+                    )
+                ),
+                SizedBox(
+                  height: 30,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+}
